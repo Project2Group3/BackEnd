6182471907a2ded6b9bc11e15d58a93a78998039
SebasX5 <sebastian.santoyopro@gmail.com>
2024-10-17 02:14:34 -0700

    Connect API to DB
    Having issues with create caused by incorrectly defined DB. Appears that id's do not have auto increment enabled.

--- publish/projectApp/models.py
+++ publish/projectApp/models.py
@@ -2,27 +2,29 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    id= models.AutoField(primary_key=True)
     name= models.CharField(max_length=250)
     email= models.CharField(max_length=250)
     username= models.CharField(max_length=250)
     image= models.CharField(max_length=250,null=True, blank=True)
     is_Admin = models.BooleanField(default=False)
-    id= models.AutoField(primary_key=True)
     def __str__(self):
         return self.username
-
-
+    class Meta:
+        db_table = 'User'  
 class UserItemList(models.Model):
+    list_id= models.AutoField(primary_key=True)
     user= models.ForeignKey('User', on_delete=models.CASCADE)
     item= models.ForeignKey('Item', on_delete=models.CASCADE)
     list_name = models.CharField(max_length=250)
-    listId= models.AutoField(primary_key=True)
     def __str__(self):
         return self.listName
+    class Meta:
+        db_table = 'user_list'  
 
 
 class Item(models.Model):
-    itemId= models.AutoField(primary_key=True)
+    item_id= models.AutoField(primary_key=True)
     price= models.FloatField()
     name = models.CharField(max_length=250)
     url = models.CharField(max_length=250)
@@ -30,3 +32,6 @@ class Item(models.Model):
     description = models.CharField(max_length=250, null=True, blank=True)
     def __str__(self):
         return self.name
+    class Meta:
+        db_table = 'Item'  
+
--- publish/projectApp/test/add_test_user.py
+++ publish/projectApp/test/add_test_user.py
@@ -5,17 +5,32 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'publish.settings')
 django.setup()
 
 # Import the User model
-from django.contrib.auth.models import User
-
+from models import User
+from serializers import UserSerializer
 # Create a test user
 def create_test_user():
-    if not User.objects.filter(username='testuser').exists():
-        user = User.objects.create_user(username='testuser', password='testpassword')
-        user.first_name = 'Test'
-        user.last_name = 'User'
-        user.email = 'testuser@example.com'
-        user.save()
-        print("Test user created:", user)
+    if not User.objects.filter(username='tester1').exists():
+        request={
+            "name": "BOB",
+            "email": "bob@example.com",
+            "username": "tester1"
+            }
+        serializer= UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        # user= UserSerializer(({
+        #     "name": "BOB",
+        #     "email": "bob@example.com",
+        #     "username": "tester1"
+        #     }
+        # ).data)
+        # user = User.objects.create_user(username='testuser', password='testpassword')
+        # user.name = 'Tester1'
+        # user.email = 'testuser@example.com'
+        # user.username= 'testuser'
+
+        # user.save()
+        print("Test user created:", serializer)
     else:
         print("User already exists.")
 
